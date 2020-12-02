25d124bbda1d18f30a78e56a4030ef6af652f299
Patrizio Pellizzon <p.pellizzon@issm.it>
2020-12-02 12:19:02 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,93 @@
+def letteraO():
+    basic.pause(int_lungo)
+    basic.show_leds("""
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        """)
+    basic.pause(int_lungo)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+def letteraS():
+    basic.pause(int_corto)
+    basic.show_leds("""
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        """)
+    basic.pause(int_corto)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+int_lungo = 0
+int_corto = 0
+int_corto = 100
+int_lungo = 1000
+
+def on_forever():
+    for index in range(3):
+        basic.pause(int_corto)
+        basic.show_leds("""
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            """)
+        basic.pause(int_corto)
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            """)
+    for index2 in range(3):
+        basic.pause(int_lungo)
+        basic.show_leds("""
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            """)
+        basic.pause(int_lungo)
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            """)
+    for index3 in range(3):
+        basic.pause(int_corto)
+        basic.show_leds("""
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            """)
+        basic.pause(int_corto)
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            """)
+    basic.show_string("fine")
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,99 @@
+// funzione per creare il punto corto
+function longPoint () {
+    basic.pause(int_lungo)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(int_lungo)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
+// funzione per creare il punto corto
+function shortPoint () {
+    basic.pause(int_corto)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(int_corto)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
+let int_lungo = 0
+let int_corto = 0
+int_corto = 100
+int_lungo = 1000
+basic.forever(function () {
+    for (let index = 0; index < 3; index++) {
+        basic.pause(int_corto)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(int_corto)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    for (let index = 0; index < 3; index++) {
+        basic.pause(int_lungo)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(int_lungo)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    for (let index = 0; index < 3; index++) {
+        basic.pause(int_corto)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(int_corto)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    basic.showString("fine")
+})
